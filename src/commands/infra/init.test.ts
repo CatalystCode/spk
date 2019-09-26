15b01146f5d949f0a2feba708516585c3352446a
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-09-26 08:36:18 -0700

    Update init.test.ts

--- src/commands/infra/init.test.ts
+++ src/commands/infra/init.test.ts
@@ -1,9 +1,4 @@
 import child_process from "child_process";
-import fs from "fs";
-import os from "os";
-import path from "path";
-import shell from "shelljs";
-import uuid from "uuid/v4";
 import {
   disableVerboseLogging,
   enableVerboseLogging,
@@ -25,9 +20,8 @@ describe("Validating executable prerequisites", () => {
   test("Validate that array of executables do not exists in PATH", async () => {
     // Iterate through an array of non-existent binaries to create a force fail. If fails, then test pass
     let fakeBinaries: string[] = ["ydawgie"];
-    validatePrereqs(fakeBinaries).then((value: boolean) => {
-      expect(value).toBeFalsy();
-    });
+    const value = await validatePrereqs(fakeBinaries);
+    expect(value).toBe(false);
   });
 });
 
@@ -35,9 +29,8 @@ describe("Validating Azure authentication", () => {
   test("Validate that a logged out user produces a force fail", async () => {
     // Produce an error that requires user to login
     child_process.exec("az logout");
-    validateAzure().then((value: boolean) => {
-      expect(value).toBeFalsy();
-    });
+    const value = await validateAzure();
+    expect(value).toBe(false);
   });
 });
 
@@ -46,8 +39,7 @@ describe("Validating environment variables", () => {
     // Set environment variables to null, and create a force fail scenario
     let variables: string[] = ["ydawgie"];
     process.env["ydawgie"] = "";
-    validateEnvVariables(variables).then((value: boolean) => {
-      expect(value).toBeFalsy();
-    });
+    const value = await validateEnvVariables(variables);
+    expect(value).toBe(false);
   });
 });
