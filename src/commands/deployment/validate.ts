a6296ed547d2a53054eb1f9645341ff5ee484c8c
edaena <edaena@outlook.com>
2019-10-08 10:39:58 -0700

    Update error message

--- src/commands/deployment/validate.ts
+++ src/commands/deployment/validate.ts
@@ -22,43 +22,43 @@ export const validateCommandDecorator = (command: commander.Command): void => {
  * Validates that the deployment configuration is specified.
  */
 export const isValidConfig = (): boolean => {
-  let missingConfig = "";
+  const missingConfig = [];
 
   if (!config.deployment) {
-    missingConfig = missingConfig + "deployment";
+    missingConfig.push("deployment");
   } else {
     if (!config.deployment.storage) {
-      missingConfig = missingConfig + "config.deployment.storage";
+      missingConfig.push("config.deployment.storage");
     } else {
       if (!config.deployment.storage.account_name) {
-        missingConfig =
-          missingConfig + "config.deployment.storage.account_name";
+        missingConfig.push("config.deployment.storage.account_name");
       }
       if (!config.deployment.storage.key) {
-        missingConfig = missingConfig + "config.deployment.storage.key";
+        missingConfig.push("config.deployment.storage.key");
       }
       if (!config.deployment.storage.partition_key) {
-        missingConfig =
-          missingConfig + "config.deployment.storage.partition_key";
+        missingConfig.push("config.deployment.storage.partition_key");
       }
       if (!config.deployment.storage.table_name) {
-        missingConfig = missingConfig + "config.deployment.storage.table_name";
+        missingConfig.push("config.deployment.storage.table_name");
       }
     }
     if (!config.deployment.pipeline) {
-      missingConfig = missingConfig + "config.deployment.pipeline ";
+      missingConfig.push("config.deployment.pipeline");
     } else {
       if (!config.deployment.pipeline.org) {
-        missingConfig = missingConfig + "config.deployment.pipeline.org ";
+        missingConfig.push("config.deployment.pipeline.org");
       }
       if (!config.deployment.pipeline.project) {
-        missingConfig = missingConfig + "config.deployment.pipeline.project";
+        missingConfig.push("config.deployment.pipeline.project");
       }
     }
   }
 
-  if (missingConfig !== "") {
-    logger.error("Validation failed. Missing configuration: " + missingConfig);
+  if (missingConfig.length > 0) {
+    logger.error(
+      "Validation failed. Missing configuration: " + missingConfig.join(" ")
+    );
     return false;
   }
 
