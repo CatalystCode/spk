28155c7867c6fc318584894d9442606cf850cdaf
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-10-15 15:38:06 -0700

    remove generate

--- src/commands/infra/index.ts
+++ src/commands/infra/index.ts
@@ -1,16 +1,10 @@
 import { Command } from "../command";
 import { createCommandDecorator } from "./create";
-import { generateCommandDecorator } from "./generate";
 import { scaffoldCommandDecorator } from "./scaffold";
 import { validateCommandDecorator } from "./vaildate";
 
 export const infraCommand = Command(
   "infra",
   "Manage and modify your Bedrock infrastructure.",
-  [
-    createCommandDecorator,
-    generateCommandDecorator,
-    scaffoldCommandDecorator,
-    validateCommandDecorator
-  ]
+  [createCommandDecorator, scaffoldCommandDecorator, validateCommandDecorator]
 );
