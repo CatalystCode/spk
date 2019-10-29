c1cf5326c35643eb338750c85707945a0148f1e3
nrose <rose.nathanielm@gmail.com>
2019-10-29 11:59:33 -0700

    linting

--- src/commands/infra/index.ts
+++ src/commands/infra/index.ts
@@ -1,10 +1,10 @@
 import { Command } from "../command";
+import { generateCommandDecorator } from "./generate";
 import { scaffoldCommandDecorator } from "./scaffold";
 import { validateCommandDecorator } from "./validate";
-import { generateCommandDecorator } from "./generate";
 
 export const infraCommand = Command(
   "infra",
   "Manage and modify your Bedrock infrastructure.",
-  [scaffoldCommandDecorator, validateCommandDecorator, generateCommandDecorator]
+  [generateCommandDecorator, scaffoldCommandDecorator, validateCommandDecorator]
 );
