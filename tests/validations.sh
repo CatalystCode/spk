b7d0b809c167c0adf84ad26329c5001a5da4a924
Andre Briggs <andrebriggs@users.noreply.github.com>
2019-11-13 21:24:32 -0800

    inital commit

--- tests/validations.sh
+++ tests/validations.sh
@@ -131,8 +131,8 @@ echo "The remote_repo_url is $remote_repo_url"
 repo_url=$(getHostandPath "$remote_repo_url")
 
 # We need to manipulate the remote url to insert a PAT token so we can
-git commit -m "inital commit"
 git remote rm origin
+git commit -m "inital commit"
 git remote add origin https://service_account:$ACCESS_TOKEN_SECRET@$repo_url
 echo "git push"
 git push -u origin --all
