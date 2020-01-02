dba8fc1929e0e5b336c3ab921289d48f95fd50f9
edaena <edaena@outlook.com>
2020-01-02 12:48:47 -0800

    Add test for hld init

--- src/commands/hld/init.test.ts
+++ src/commands/hld/init.test.ts
@@ -8,6 +8,8 @@ import {
   logger
 } from "../../logger";
 import { initialize } from "./init";
+import { checkoutCommitPushCreatePRLink } from "../../lib/gitutils";
+jest.mock("../../lib/gitutils");
 
 beforeAll(() => {
   enableVerboseLogging();
@@ -39,5 +41,31 @@ describe("Initializing an HLD Repository.", () => {
     for (const filepath of filepaths) {
       expect(fs.existsSync(filepath)).toBe(true);
     }
+  });
+
+  test("New directory is created and git push is enabled.", async () => {
+    // Create random directory to initialize
+
+    const randomTmpDir = path.join(os.tmpdir(), uuid());
+    fs.mkdirSync(randomTmpDir);
+
+    logger.info(`creating randomTmpDir ${randomTmpDir}`);
+
+    // addService call
+    await initialize(randomTmpDir, true);
+
+    // Check temp test directory exists
+    expect(fs.existsSync(randomTmpDir)).toBe(true);
+
+    // Verify new azure-pipelines created
+    const filepaths = ["manifest-generation.yaml", "component.yaml"].map(
+      filename => path.join(randomTmpDir, filename)
+    );
+
+    for (const filepath of filepaths) {
+      expect(fs.existsSync(filepath)).toBe(true);
+    }
+
+    expect(checkoutCommitPushCreatePRLink).toHaveBeenCalled();
   });
 });
