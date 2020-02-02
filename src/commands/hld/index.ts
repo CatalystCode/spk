e4bead7c987e2067fa06706f8fd34cc5d8de946c
Dennis Seah <dennis.seah@gmail.com>
2020-02-02 07:53:43 -0800

    stage

--- src/commands/hld/index.ts
+++ src/commands/hld/index.ts
@@ -1,7 +1,7 @@
 import { Command } from "../command";
 import { commandDecorator as initCommandDecorator } from "./init";
 import { installHldToManifestPipelineDecorator } from "./pipeline";
-import { reconcileHldDecorator } from "./reconcile";
+import { commandDecorator as reconcileHldDecorator } from "./reconcile";
 export const hldCommand = Command(
   "hld",
   "Commands for initalizing and managing a bedrock HLD repository.",
