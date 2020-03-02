bcffc2613fc87f92bf9397e1710fbca55b80a0ec
Samiya Akhtar <samiyaakhtar7@gmail.com>
2020-03-02 15:58:23 -0800

    Remove -it

--- src/commands/deployment/dashboard.ts
+++ src/commands/deployment/dashboard.ts
@@ -140,7 +140,6 @@ export const launchDashboard = async (
     const containerId = await exec("docker", [
       "run",
       "-d",
-      "-it",
       "--rm",
       ...(await getEnvVars(config)),
       "-p",
