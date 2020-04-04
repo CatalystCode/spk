83d2ea33be50262114224f399acc888dec1e8e37
Dennis Seah <dennis.seah@gmail.com>
2020-04-03 19:53:07 -0700

    [FEATURE] inheritence information in operational doc

--- src/lib/commandBuilder.ts
+++ src/lib/commandBuilder.ts
@@ -4,6 +4,8 @@ import { Logger, transports } from "winston";
 import { logger } from "../logger";
 import { ConfigYaml } from "../types";
 import { hasValue } from "./validator";
+import { build as buildError } from "./errorBuilder";
+import { errorStatusCode } from "./errorStatusCode";
 
 /**
  * Command Option
@@ -120,7 +122,10 @@ export const validateForRequiredValues = (
   const errors = missingItems.map((item) => item.opt.arg);
 
   if (toThrow && errors.length !== 0) {
-    throw `The following arguments are required:\n ${errors.join("\n ")}`;
+    throw buildError(errorStatusCode.VALIDATION_ERR, {
+      errorKey: "validation-err-missing-vals",
+      values: [errors.join("\n ")],
+    });
   }
 
   if (errors.length !== 0) {
